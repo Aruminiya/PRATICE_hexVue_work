cb127908af825c12f15447fa3a1daf1f9a8d7bf3
Leo <leo80103@gmail.com>
2024-01-08 13:35:26 +0800

    week1 作業使用 pinia

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+export default {};
 </script>
 
 <template>
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/Week1.vue"),
+      component: () => import("../views/Week1View.vue"),
     },
   ],
 });
